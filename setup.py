34d5c19077a4565d38e232b6456b3702398d7668
ccw7463 <changwoo7463@gmail.com>
2024-09-10 11:13:04 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CFDSimulator',
+    version='0.0.1',
+    packages=find_packages(include=['CFDSimulator']),
+    install_requires=[
+    ],
+    description="CFDSimulator Repository"
+)
